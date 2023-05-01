ad8bbe1903396b1776dd915f816f024e5553adb7
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-05-01 20:59:25 +0300

    remove console.log

--- index.js
+++ index.js
@@ -11,17 +11,6 @@ function crawlTree(tree) {
 	return string;
 }
 
-/* console.log(
-	crawlTree([
-		[
-			[["П", "р"], "и"],
-			["в", ["е", "т"]],
-		],
-		[["и", "к"], "и"],
-	])
-); */
-// console.log(crawlTree([["П", "и"], ["с", ["т", ["о"]]], "л", [["е", "т"], [["и", "к"]]], "и"]));
-
 // Код для второго задания напиши в этой функции
 function twoSum(arr, target, start = 0) {
 	let indexes = [];
@@ -36,10 +25,6 @@ function twoSum(arr, target, start = 0) {
 	}
 	return indexes;
 }
-
-// console.log(twoSum([3, 2, 4], 6));
-// console.log(twoSum([2, 7, 11, 15], 9));
-// console.log(twoSum([3, 3], 6));
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
